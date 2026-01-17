f8f7915e8cca88ab4c6c28f47d5c1e48626fb673
Anıl Sevinç <contact@anilsevinc.net>
2026-01-17 04:55:16 +0300

    Soru 2 için sipariş işleme ve toplam tutar hesaplama higher order fonksiyonu eklendi

--- answers.js
+++ answers.js
@@ -23,8 +23,7 @@ function menudeAra(restoran, callback) {
         menu.urunler.forEach((urun) => {
             // Callback fonksiyonu ile ürünü test edip koşulun sağlanması durumunda eklenir
             if (callback(urun)) {
-                sonuclar.push({
-                    ...urun, // Ürünün tüm özelliklerini kopyalıyoruz ve kategori bilgisini ekliyoruz
+                sonuclar.push({...urun, // Ürünün tüm özelliklerini kopyalıyoruz ve kategori bilgisini ekliyoruz
                     kategori: menu.kategori
                 });
             }
@@ -45,3 +44,101 @@ console.log("500 kalorinin altındaki ürünler:", azKalorili);
 console.log('"domates" malzemesi içeren ürünler:', domatesliUrunler);
 */
 
+
+/**
+ SORU 2: Sipariş İşleme Sistemi
+Görev: Siparişleri detaylı şekilde işleyen bir sistem oluştur.
+İstenenler:
+
+- siparisiIsle higher-order function'ı yaz
+- Her sipariş ürününü menüde bulup detaylarını getir
+- Callback ile sipariş detayını özelleştirilebilir yap
+- Sipariş toplam tutarını hesapla
+
+ */
+
+// ÇÖZÜM 2: Sipariş İşleme Sistemi
+// Veri yapısı
+/* restoran.siparisler -> array , array içinde obje,
+obje içinde masa, garson, saat ve durum key, urunler ise array*/
+
+// Sipariş objesi için  için siparis, siparişteki ürünler için restoran ve özelleştirmek için callback parametreleri
+function siparisIsle(siparis, restoran, callback) {
+    const tumUrunler = [];    // Tüm ürünleri tutması için array
+    // Restorandaki tüm menüleri dolaşması için forEach
+    restoran.menuler.forEach((menu) => {
+        // Menüdeki ürünleri dolaşma - urun burada array ekleyeceği ise obje
+        menu.urunler.forEach((urun) => {
+            tumUrunler.push({...urun, kategori: menu.kategori}); // Kategoriyi de ekleyerek yeni obje
+        });
+
+    });
+    //console.log(tumUrunler); // Test
+    
+    // çıktı olarak dönecek obje 
+    // parametre olarak verdiğimiz sipraisten burada bilgiler alınacak
+    const siparisDetayi = {
+        masa: siparis.masa,
+        garson: siparis.garson,
+        saat: siparis.saat,
+        durum: siparis.durum,
+        urunler: [],
+        toplamTutar: 0
+    };
+    // Sipariş ürünlerinde dolaşma - urunler burada array - arrayin içindeki ürün özelliklerine bakacak
+    siparis.urunler.forEach((siparisUrunu) => {
+        // sipariste sadece id ve adet var bize adı fiyatı kalorisi vb özellikleri de lazım
+        // tüm ürünlerden sipariş ürününün idsine göre ürünü bulma
+        const bulunanUrun = tumUrunler.find(menuUrunu => menuUrunu.id === siparisUrunu.urunId);
+
+        // Eğer o ürün varsayı kontrol edelim
+        if (bulunanUrun) {
+            const urununDetayi = {
+            // toplam fiyatına ihtiyacımız var fiyatı ve adetini çarparak bulabiliriz
+            urununToplamFiyati: bulunanUrun.fiyat * siparisUrunu.adet,
+            //siparis objesinden alınacak
+            adet: siparisUrunu.adet,
+            // diğer özellikleri - menuden alınan
+            id: bulunanUrun.id,
+            ad: bulunanUrun.ad,
+            fiyat: bulunanUrun.fiyat,
+            kalori: bulunanUrun.kalori,
+            malzemeler: bulunanUrun.malzemeler,
+            kategori: bulunanUrun.kategori
+            };
+            
+            //console.log(urununDetayi); // Test
+            // 2 farklı yerden veri alıp objeyi oluşturduk
+
+            // objeyi sipariş detayına ekliyoruz
+            siparisDetayi.urunler.push(urununDetayi);
+            // her eklenen ürünün fiyatını siparişin toplamına ekliyoruz
+            siparisDetayi.toplamTutar += urununDetayi.urununToplamFiyati;
+          
+        }
+    });
+    
+    // Özelleştirebilme için callback kontrolü ekliyoruz ve fonksiyopnu higher-order yapıyoruz
+    if (callback) {
+        return callback(siparisDetayi);
+    }
+    return siparisDetayi;
+
+}
+
+/* Testler 
+const sonuc = siparisIsle(restoran.siparisler[0], restoran);
+console.log(sonuc);
+
+// siparişe indirim uygulayalım
+const indirimli = siparisIsle(
+    restoran.siparisler[0],
+    restoran,
+    (siparis) => {
+        siparis.toplamTutar *= 0.9;
+        return siparis;
+    }
+);
+
+console.log(indirimli);
+*/
